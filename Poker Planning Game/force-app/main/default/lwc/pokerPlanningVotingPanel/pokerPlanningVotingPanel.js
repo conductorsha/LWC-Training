711b765d8616180997d8896c0c5264f44c3905cd
taras.buchynskyi <taras.buchynskyi@canon-europe.com>
2022-10-16 15:01:15 +0200

    remove reference to js files

--- Poker Planning Game/force-app/main/default/lwc/pokerPlanningVotingPanel/pokerPlanningVotingPanel.js
+++ Poker Planning Game/force-app/main/default/lwc/pokerPlanningVotingPanel/pokerPlanningVotingPanel.js
@@ -2,7 +2,6 @@ import { LightningElement } from "lwc";
 
 export default class PokerPlanningVotingPanel extends LightningElement {
     voteOptions = ["0.5", "1", "2", "3", "5", "8", "13", "21", "34", "55", "89", "100"];
-    voteOption = "0.5";
 
     handleVoteOptionClick(event) {
         this.resetCardsStyles();
